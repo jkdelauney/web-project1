f1062f86d710a7c9538c8e302ed4a5dc1fd13a0e
Jody Delauney <jodydelauney@gmail.com>
2020-05-14 21:24:36 -0400

    Updated to reflect column names

--- import.py
+++ import.py
@@ -8,12 +8,12 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    f = open("flights.csv")
+    f = open("books.csv")
     reader = csv.reader(f)
-    for origin, destination, duration in reader:
-        db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
-                   {"origin": origin, "destination": destination, "duration": duration})
-        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+    for isbn, title, author, year in reader: # isbn, title, author, year
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                   {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(f"Added isbn from {isbn} titled {title} by {author} published {year}.")
     db.commit()
 
 if __name__ == "__main__":
