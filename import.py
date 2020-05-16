a1e98e1aab75fa9ec7ac6a1430617bd377c84a44
Jody Delauney <jodydelauney@gmail.com>
2020-05-15 21:35:22 -0400

    Stopped displaying each imported row and started showing a . per row

--- import.py
+++ import.py
@@ -1,5 +1,4 @@
-import csv
-import os
+import csv, os, sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -8,14 +7,18 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
+    r_count = 0
     f = open("books.csv")
     reader = csv.reader(f)
     next(reader)
     for isbn, title, author, year in reader: # isbn, title, author, year
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                    {"isbn": isbn, "title": title, "author": author, "year": year})
-        print(f"ISBN: {isbn}, title: {title}, by: {author}, year: {year}.")
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        r_count += 1
     db.commit()
+    print(f"\nRecords imported: {r_count}")
 
 if __name__ == "__main__":
     main()
