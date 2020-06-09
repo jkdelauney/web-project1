e197e69245d7087586d9a8c41107ce6a81c0e14a
Jody Delauney <jodydelauney@gmail.com>
2020-06-09 19:19:08 -0400

    Code cleanup, no functionality changes

--- import.py
+++ import.py
@@ -15,15 +15,18 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     # db.execute("DROP TABLE IF EXISTS books CASCADE")
     db.execute(
-        "CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY,isbn varchar(13) NOT NULL, title character varying NOT NULL, author character varying NOT NULL, year character(4) NOT NULL)")
+        "CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY,isbn varchar(13) NOT NULL, title character varying NOT NULL, author character varying NOT NULL, year character(4) NOT NULL)"
+    )
 
     r_count = 0
     f = open("books.csv")
     reader = csv.reader(f)
     next(reader)  # advance past column headers
     for isbn, title, author, year in reader:  # isbn, title, author, year
-        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-                   {"isbn": isbn, "title": title, "author": author, "year": year})
+        db.execute(
+            "INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+            {"isbn": isbn, "title": title, "author": author, "year": year},
+        )
         sys.stdout.write(".")
         sys.stdout.flush()
         r_count += 1
